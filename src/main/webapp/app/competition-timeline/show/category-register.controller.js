c7540038352f7d5870cf2a7b54c52f6edfbe7746
Dmitry <dimonn12@hotmail.com>
2016-10-12 01:34:04 +0300

    some minor changes

--- src/main/webapp/app/competition-timeline/show/category-register.controller.js
+++ src/main/webapp/app/competition-timeline/show/category-register.controller.js
@@ -1,13 +1,13 @@
-(function() {
+(function () {
     'use strict';
 
     angular
         .module('danceFormApp')
         .controller('CategoryRegisterController', CategoryRegisterController);
 
-    CategoryRegisterController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Competition', 'CompetitionCategory', 'DanceClass', 'AlertService'];
+    CategoryRegisterController.$inject = ['$scope', '$rootScope', '$stateParams', 'previousState', 'entity', 'Competition', 'CompetitionCategory', 'DanceClass', 'RegisteredCouple', 'AlertService'];
 
-    function CategoryRegisterController($scope, $rootScope, $stateParams, previousState, entity, Competition, CompetitionCategory, DanceClass, AlertService) {
+    function CategoryRegisterController($scope, $rootScope, $stateParams, previousState, entity, Competition, CompetitionCategory, DanceClass, RegisteredCouple, AlertService) {
         var vm = this;
 
         vm.currentCompetition = entity;
@@ -16,6 +16,10 @@
         vm.availableCategories = [];
 
         vm.danceClasses = [];
+
+        vm.registerCouple = {};
+        
+        vm.save = save;
 
         load();
         loadClasses();
@@ -33,17 +37,31 @@
 
         function loadClasses() {
             DanceClass.query({}, onSuccess, onError);
-                    function onSuccess(data, headers) {
-                        vm.danceClasses = data;
-                    }
+            function onSuccess(data, headers) {
+                vm.danceClasses = data;
+            }
 
-                    function onError(error) {
-                        AlertService.error(error.data.message);
-                    }
+            function onError(error) {
+                AlertService.error(error.data.message);
+            }
         }
 
         function update() {
             console.log('update');
+        }
+
+        function save() {
+            RegisteredCouple.save(vm.registerCouple, onSaveSuccess, onSaveError);
+            function onSaveSuccess (result) {
+                $scope.$emit('danceFormApp:registeredCoupleUpdate', result);
+                vm.isSaving = false;
+            }
+
+            function onSaveError () {
+                vm.isSaving = false;
+                AlertService.error(error.data.message);
+            }
+
         }
     }
 })();
