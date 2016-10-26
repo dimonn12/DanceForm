6b6ead7a1adde0a163723de22e4c2305b604d1d1
Dmitry <dimonn12@hotmail.com>
2016-10-26 19:49:10 +0300

    added dance classes on Competition categories page. Fixes some bugs

--- src/main/webapp/app/competitions/competition/competition-category/competition-category.controller.js
+++ src/main/webapp/app/competitions/competition/competition-category/competition-category.controller.js
@@ -5,20 +5,40 @@
 		.module('danceFormApp')
 		.controller('CompetitionCategoryController', CompetitionCategoryController);
 
-	CompetitionCategoryController.$inject = ['$scope', '$state', '$stateParams', 'previousState', 'CompetitionCategory'];
+	CompetitionCategoryController.$inject = ['$scope', '$state', '$stateParams', 'previousState', 'CompetitionCategory', 'DanceClass'];
 
-	function CompetitionCategoryController($scope, $state, $stateParams, previousState, CompetitionCategory) {
+	function CompetitionCategoryController($scope, $state, $stateParams, previousState, CompetitionCategory, DanceClass) {
 		var vm = this;
 
 		vm.previousState = previousState;
 
 		vm.competitionCategories = [];
+		vm.danceClasses = DanceClass.query();
 
 		loadAll();
 
 		function loadAll() {
 			CompetitionCategory.query({competitionId: $stateParams.competitionId}, function(result) {
 				vm.competitionCategories = result;
+
+				for(var i = 0; i < vm.competitionCategories.length; i++) {
+					if(null != vm.competitionCategories[i].maxDanceClass.id) {
+						vm.competitionCategories[i].danceClasses = [];
+						var maxDanceClass = null;
+						for(var j = 0; j < vm.danceClasses.length; j++) {
+							if(vm.danceClasses[j].id == vm.competitionCategories[i].maxDanceClass.id) {
+								maxDanceClass = vm.danceClasses[j];
+								break;
+							}
+						}
+						for(var j = 0; j < vm.danceClasses.length; j++) {
+							if(vm.danceClasses[j].weight < maxDanceClass.weight) {
+								vm.competitionCategories[i].danceClasses.push(vm.danceClasses[j]);
+							}
+						}
+					}
+				}
+
 			});
 		}
 	}
