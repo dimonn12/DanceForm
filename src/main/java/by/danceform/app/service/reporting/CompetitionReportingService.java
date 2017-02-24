479349de4be028403f1bc428f065913801a497b9
Dmitry Shanko <dimonn12@hotmail.com>
2017-02-24 10:36:38 +0300

    Apache poi

--- src/main/java/by/danceform/app/service/reporting/CompetitionReportingService.java
+++ src/main/java/by/danceform/app/service/reporting/CompetitionReportingService.java
@@ -1,7 +1,15 @@
 package by.danceform.app.service.reporting;
 
+import by.danceform.app.domain.couple.RegisteredCouple;
+import by.danceform.app.dto.competition.CompetitionCategoryDTO;
+import by.danceform.app.dto.competition.CompetitionDTO;
+import by.danceform.app.dto.couple.RegisteredCoupleDTO;
+import by.danceform.app.service.competition.CompetitionCategoryService;
 import by.danceform.app.service.competition.CompetitionService;
+import by.danceform.app.service.couple.RegisteredCoupleService;
+import java.util.List;
 import javax.inject.Inject;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
 /**
@@ -13,7 +21,24 @@ public class CompetitionReportingService {
     @Inject
     private CompetitionService competitionService;
 
+    @Inject
+    private CompetitionCategoryService competitionCategoryService;
+
+    @Inject
+    private RegisteredCoupleService registeredCoupleService;
+
     public Object getCompetitionReport(Long competitionId) {
+        CompetitionDTO comp = competitionService.findOne(competitionId);
+        if (null == comp) {
+            return null;
+        }
+        String competitionName = comp.getName();
+        List<CompetitionCategoryDTO> competitionCategories = competitionCategoryService.findByCompetitionId(competitionId);
+        for (CompetitionCategoryDTO categoryDTO : competitionCategories) {
+            String categoryName = categoryDTO.getName();
+            List<RegisteredCoupleDTO> registeredCouples = registeredCoupleService.findByCategoryId(categoryDTO.getId());
+            XSSFWorkbook workBook = new XSSFWorkbook();
+        }
         return null;
     }
 }
