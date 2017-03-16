75e85ca889d3f055e060bc259de2e58e1c7c79dd
Dmitry <dimonn12@hotmail.com>
2017-03-17 00:53:15 +0300

    Refactoring in changesets. More content in changesets.

--- src/main/resources/sql/initial.sql
+++ src/main/resources/sql/initial.sql
@@ -2,7 +2,7 @@ SET @@global.innodb_large_prefix = 1;
 SET @@global.innodb_file_format=barracuda;
 SET @@global.innodb_file_per_table=true;
 
-drop schema if exists `DanceForm`;
+drop schema if exists `Dance_Form`;
 
-create database if not exists DanceForm;
-ALTER SCHEMA `DanceForm`  DEFAULT CHARACTER SET utf8;
+create database if not exists Dance_Form;
+ALTER SCHEMA `Dance_Form`  DEFAULT CHARACTER SET utf8;
