dbb91b6e242770318a42432ff86adf8f3ce0b0d1
Dmitry Shanko <dimonn12@hotmail.com>
2017-03-20 17:36:16 +0300

    Added soft deleted @where clause

--- src/main/java/by/danceform/app/domain/SoftDeletedEntity.java
+++ src/main/java/by/danceform/app/domain/SoftDeletedEntity.java
@@ -3,8 +3,10 @@ package by.danceform.app.domain;
 import java.io.Serializable;
 import javax.persistence.Column;
 import javax.persistence.MappedSuperclass;
+import org.hibernate.annotations.Where;
 
 @MappedSuperclass
+@Where(clause = "deleted <> 1")
 public abstract class SoftDeletedEntity<ID extends Serializable>
     extends AbstractAuditingEntity<ID> {
 
