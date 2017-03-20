d0c1ccf564c92dfe0dc6097dd66539bcf9880627
Dmitry Shanko <dimonn12@hotmail.com>
2017-03-20 19:11:19 +0300

    More about SoftDelete func

--- src/main/java/by/danceform/app/repository/SoftDeletedEntityRepository.java
+++ src/main/java/by/danceform/app/repository/SoftDeletedEntityRepository.java
@@ -2,6 +2,7 @@ package by.danceform.app.repository;
 
 import by.danceform.app.domain.SoftDeletedEntity;
 import java.io.Serializable;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.NoRepositoryBean;
 
@@ -9,8 +10,11 @@ import org.springframework.data.repository.NoRepositoryBean;
 public interface SoftDeletedEntityRepository<T extends SoftDeletedEntity<ID>, ID extends Serializable>
     extends AbstractAuditingEntityRepository<T, ID> {
 
-    @Override
-    @Query("select e from #{#entityName} e where e.deleted=false AND e.id = ?1")
-    T findOne(ID id);
+    @Query("select e from #{#entityName} e where e.deleted=true AND e.id = ?1")
+    T findDeletedEntity(ID id);
+
+    @Modifying
+    @Query("update #{#entityName} e set e.deleted=true where e.id=?1")
+    void softDelete(ID id);
 
 }
--- src/test/java/by/danceform/app/repository/AbstractSoftDeletedEntityRepositoryTest.java
+++ src/test/java/by/danceform/app/repository/AbstractSoftDeletedEntityRepositoryTest.java
@@ -1,9 +1,29 @@
 package by.danceform.app.repository;
 
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
 import by.danceform.app.domain.SoftDeletedEntity;
 import java.io.Serializable;
+import org.junit.Test;
 
 public abstract class AbstractSoftDeletedEntityRepositoryTest<R extends SoftDeletedEntityRepository<E, ID>, E extends
     SoftDeletedEntity<ID>, ID extends Serializable> extends AbstractAuditingEntityRepositoryTest<R, E, ID> {
 
+    private static final Boolean DELETED_FALSE = Boolean.FALSE;
+    private static final Boolean DELETED_TRUE = Boolean.TRUE;
+
+    @Test
+    public void testSoftDelete() {
+        final E createdEntity = getRepository().save(getNewEntity());
+        final ID entityId = createdEntity.getId();
+        assertThat(createdEntity.getDeleted(), equalTo(DELETED_FALSE));
+        getRepository().softDelete(entityId);
+        final E deletedEntityShouldNotExist = getRepository().findOne(entityId);
+        final E deletedEntityShouldExist = getRepository().findDeletedEntity(entityId);
+        assertThat(deletedEntityShouldNotExist, nullValue());
+        assertThat(deletedEntityShouldExist, not(nullValue()));
+    }
 }
